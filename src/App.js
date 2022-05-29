530326a5b5cdb894c6bb1bbc1766fdcc20555b8b
Sathnindu Kottage <sathnidukottage@gmail.com>
2022-05-29 14:20:41 +0530

    privacy and terms pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/layouts/Navbar";
 import NotFound from "./components/layouts/NotFound";
+import Privacy from "./components/Privacy";
+import Terms from "./components/Terms";
 // import Footer from "./components/layouts/Footer";
 
 class App extends Component {
@@ -16,6 +18,8 @@ class App extends Component {
           <div className="container" style={{marginTop: "57px"}}>
             <Switch>
               <Route exact path={["/"]} component={Home}/>
+              <Route exact path={["/privacy"]} component={Privacy}/>
+              <Route exact path={["/terms"]} component={Terms}/>
               <Route component={NotFound}/>
             </Switch>
           </div>
--- src/components/layouts/NotFound.js
+++ src/components/layouts/NotFound.js
@@ -1,11 +1,20 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom";
 
 export default class NotFound extends Component {
     render() {
         return (
             <div>
                 <div className="container">
-                    <h1>404 - Not found</h1>
+                    <div className={"not-found con-mid"}>
+
+                        <div className={"not-found-head"}>
+                            <h1><span style={{color: "#000"}}>error</span></h1>
+                            <h1>&nbsp;</h1>
+                            <h1 style={{color: "#737dff"}}>404</h1>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         );
