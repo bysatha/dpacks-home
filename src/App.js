ce8f3d7708f4a3f5f5f37a41fb4df5fdfe6dbafe
Sathnindu Kottage <sathnidukottage@gmail.com>
2022-05-29 18:39:53 +0530

    react default debugging

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {Switch, Route} from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import Home from "./components/Home";
